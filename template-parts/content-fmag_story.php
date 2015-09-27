9f35193490b294e3cd06d371a32dd5db6adf1b35
Cameron Browning <camb416@gmail.com>
2015-09-27 16:30:44 -0400

    responsive work for frontpage and turnpages

--- template-parts/content-fmag_story.php
+++ template-parts/content-fmag_story.php
@@ -11,29 +11,21 @@
 ?>
 
 <article id="post-<?php the_ID();?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
 
 
-        <?php
+	<div class="entry-content">
+		<?php
 
 
-        the_content( sprintf(
-        /* translators: %s: Name of current post. */
-            wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'fantastics' ), array( 'span' => array( 'class' => array() ) ) ),
-            the_title( '<span class="screen-reader-text">"', '"</span>', false )
-        ) );
+
 
         ?>
 
-		<?php if ( 'post' == get_post_type() ) : ?>
-		<div class="entry-meta">
-			<?php fantastics_posted_on(); ?>
-		</div><!-- .entry-meta -->
-		<?php endif; ?>
 
-        <ul class="minispreads">
+
+        <ul class="medspread">
             <?php
+
 
             $attachments = get_posts( array(
                 'post_type' => 'attachment',
@@ -42,6 +34,59 @@
                 'exclude'     => get_post_thumbnail_id(),
                 'orderby'     => 'menu_order',
             ) );
+
+            if ( $attachments ) {
+                $numPages = count($attachments);
+                for ( $i = 0; $i < 2 ; $i++) {
+                    $attachment = $attachments[$i];
+                    $class = "post-attachment mime-" . sanitize_title( $attachment->post_mime_type );
+                    if($i>=2){
+                        $class .= " hidden";
+                    }
+                    $thumbimg = wp_get_attachment_image( $attachment->ID, 'thumbnail-size', true );
+                    if(0 === $i%2) echo '<li class="' . $class . ' data-design-thumbnail"><a href="'.esc_url( get_permalink() ).'">';
+                    echo ''.$thumbimg;
+                    if(0 !== $i%2) echo '</a></li>';
+                }
+
+
+            }
+
+            ?>
+        </ul>
+
+
+
+	</div><!-- .entry-content -->
+
+    <header class="entry-header">
+
+
+
+        <?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+
+
+        <?php
+        echo the_content('',false);
+
+        //        the_content( sprintf(
+        //        /* translators: %s: Name of current post. */
+        //            wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'fantastics' ), array( 'span' => array( 'class' => array() ) ) ),
+        //            the_title( '<span class="screen-reader-text">"', '"</span>', false )
+        //       ) );
+
+        ?>
+
+        <?php if ( 'post' == get_post_type() ) : ?>
+            <div class="entry-meta">
+                <?php fantastics_posted_on(); ?>
+            </div><!-- .entry-meta -->
+        <?php endif; ?>
+
+        <ul class="minispreads">
+            <?php
+
+
 
             if ( $attachments ) {
                 $numPages = count($attachments);
@@ -74,51 +119,15 @@
         }
         ?>
 
-
-	</header><!-- .entry-header -->
-
-	<div class="entry-content">
-		<?php
-
-
-
-
+        <?php
+        wp_link_pages( array(
+            'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'fantastics' ),
+            'after'  => '</div>',
+        ) );
         ?>
 
+    </header><!-- .entry-header -->
 
-
-        <ul class="medspread">
-            <?php
-            if ( $attachments ) {
-                $numPages = count($attachments);
-                for ( $i = 0; $i < 2 ; $i++) {
-                    $attachment = $attachments[$i];
-                    $class = "post-attachment mime-" . sanitize_title( $attachment->post_mime_type );
-                    if($i>=2){
-                        $class .= " hidden";
-                    }
-                    $thumbimg = wp_get_attachment_image( $attachment->ID, 'thumbnail-size', true );
-                    if(0 === $i%2) echo '<li class="' . $class . ' data-design-thumbnail"><a href="'.esc_url( get_permalink() ).'">';
-                    echo ''.$thumbimg;
-                    if(0 !== $i%2) echo '</a></li>';
-                }
-
-
-            }
-
-            ?>
-        </ul>
-
-
-
-
-		<?php
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'fantastics' ),
-				'after'  => '</div>',
-			) );
-		?>
-	</div><!-- .entry-content -->
 
 	<footer class="entry-footer">
 		<?php fantastics_entry_footer(); ?>
