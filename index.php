69eca0a6fff40afdd666ad09b567fa5ce56b9b80
Cameron Browning <camb416@gmail.com>
2015-12-01 23:53:29 -0500

    integrating index theme from HTML mockup

--- index.php
+++ index.php
@@ -13,99 +13,206 @@
  */
 
  ?>
-<?php
-$paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
-// are we on page one?
 
-if(1 == $paged && is_front_page()): ?>
-<div id="bigcover">
-<?php
-
-$mainQuery = $wp_query;
-
-
-
-
-
-/*
-// Get the last 10 posts in the special_cat category.
- query_posts( 'post_type=fmag_cover&posts_per_page=1' ); ?>
-
-<?php while ( have_posts() ) : the_post(); ?>
-    <!-- Do special_cat stuff... -->
-   <?php  get_template_part( 'template-parts/content', get_post_type() ); ?>
-<?php endwhile;
-*/
-?>
-<?php
-    $r = new WP_Query(
-    array( 'posts_per_page' => 1,
-    'no_found_rows' => true,
-    'post_status' => 'publish',
-    'ignore_sticky_posts' => true,
-    'post_type' => 'fmag_cover',
-    'tax_query' => array(
-    array(
-    'taxonomy' => 'term',
-    'field' => 'slug',
-    'terms' => array('focus'),
-        'operator' => 'NOT IN'
-
-    )
-    ) ) );
-
-    if ($r->have_posts()) : ?>
-
-
-
-        <?php while ( $r->have_posts() ) : $r->the_post(); ?>
-
-                <?php
-                // working methods here:
-                // the_permalink()
-                // esc_attr( get_the_title() ? get_the_title() : get_the_ID() );
-                // <?php if ( get_the_title() ) the_title(); else the_ID();
-                // get_the_date();
-
-
-
-                $args = array(
-                    'post_type' => 'attachment',
-                    'numberposts' => 1,
-                    'post_status' => null,
-                    'post_parent' => get_the_ID()
-                );
-
-                $attachments = get_posts( $args );
-                if ( $attachments ) {
-                    foreach ( $attachments as $attachment ) {
-                        //echo '<li>';
-                        echo wp_get_attachment_image( $attachment->ID, 'full' );
-                        //echo '<p>';
-                        echo apply_filters( 'the_title', $attachment->post_title );
-                        //echo '</p></li>';
-                    }
-                }
-                ?>
-
-        <?php endwhile; ?>
-
-
-
-
-
-
-        <?php endif; ?>
-
-
-
-<?php
-$wp_query = $mainQuery;
-?>
-</div>
-<?php endif; ?>
 
 <?php get_header(); ?>
+
+
+
+
+<?php
+///////////////////////////////////////////////////
+// Do the top bit
+///////////////////////////////////////////////////
+?>
+
+<div id="top">
+
+    <?php // top-main ?>
+
+    <div class="top-main">
+
+
+
+
+            <?php
+
+            // TODO: refactor this!
+
+            $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+            // are we on page one?
+
+            if(1 == $paged && is_front_page()): ?>
+                <div class="bigcover">
+                    <?php
+
+                    $mainQuery = $wp_query;
+
+                    ?>
+                    <?php
+                    $r = new WP_Query(
+                        array( 'posts_per_page' => 1,
+                            'no_found_rows' => true,
+                            'post_status' => 'publish',
+                            'ignore_sticky_posts' => true,
+                            'post_type' => 'fmag_cover',
+                            'tax_query' => array(
+                                array(
+                                    'taxonomy' => 'term',
+                                    'field' => 'slug',
+                                    'terms' => array('focus'),
+                                    'operator' => 'NOT IN'
+
+                                )
+                            ) ) );
+
+                    if ($r->have_posts()) : ?>
+
+
+
+                        <?php while ( $r->have_posts() ) : $r->the_post(); ?>
+
+                            <?php
+                            // working methods here:
+                            // the_permalink()
+                            // esc_attr( get_the_title() ? get_the_title() : get_the_ID() );
+                            // <?php if ( get_the_title() ) the_title(); else the_ID();
+                            // get_the_date();
+
+
+
+                            $args = array(
+                                'post_type' => 'attachment',
+                                'numberposts' => 1,
+                                'post_status' => null,
+                                'post_parent' => get_the_ID()
+                            );
+
+                            $attachments = get_posts( $args );
+                            if ( $attachments ) {
+                                foreach ( $attachments as $attachment ) {
+                                    //echo '<li>';
+                                    echo wp_get_attachment_image( $attachment->ID, 'full' );
+                                    //echo '<p>';
+                                    echo apply_filters( 'the_title', $attachment->post_title );
+                                    //echo '</p></li>';
+                                }
+                            }
+                            ?>
+
+                        <?php endwhile; ?>
+
+
+
+
+
+
+                    <?php endif; ?>
+
+
+
+                    <?php
+                    $wp_query = $mainQuery;
+                    ?>
+                </div>
+            <?php endif; ?>
+
+
+
+        <div class="menu-undercover">menu-undercover
+            <br />
+            When you scroll below this, it will snap to top of browser.
+            Contains links to Terms, Contact, etc.</div>
+        <div class="storyroll">
+            storyroll (two most recent)
+            <article>article</article>
+            <article>article</article>
+
+        </div>
+    </div>
+
+    <?php // top-side
+
+    if(1 == $paged && is_front_page()){
+        echo '    <div class="top-side">';
+        echo '        <div class="socialbuttons">
+
+
+    <p>Follow FANTASTICS on:</p>
+
+<a class="sb-f sb-f" href="https://twitter.com/fantasticsmag"><div class="sb-f sb-f-twi"></div>twitter</a>
+
+<a class="sb-f " href="http://www.facebook.com/pages/Fantastics/100542323355212"><div class="sb-f sb-f-fac"></div>facebook</a>
+
+<a class="sb-f" href="http://pinterest.com/fantasticsmag/"><div class="sb-f sb-f-pin"></div>pinterest</a>
+
+<a class="sb-f" href="http://fantasticsmag.tumblr.com/"><div class="sb-f sb-f-tum"></div>tumblr</a>
+
+<a class="sb-f" href="http://instagram.com/fantastics/"><div class="sb-f sb-f-ins"></div>instagram</a>
+
+
+        </div>';
+
+
+        if (!function_exists('dynamic_sidebar') || !dynamic_sidebar('Index Top-Side')){
+            // do nothing if not there
+            // echo "default stuff";
+        }
+//        echo '<div class="adspace">adspace</div> ';
+        echo ' </div>';
+    }
+
+    ?>
+
+
+
+
+
+
+
+ </div>
+    <?php
+///////////////////////////////////////////////////
+// Intermission A
+///////////////////////////////////////////////////
+
+//intermission A
+
+///////////////////////////////////////////////////
+// Middle
+///////////////////////////////////////////////////
+
+// mid-main
+
+// mid-side
+
+
+///////////////////////////////////////////////////
+// Intermission B
+///////////////////////////////////////////////////
+
+// intermission b
+
+///////////////////////////////////////////////////
+// Bottom
+///////////////////////////////////////////////////
+
+// bottom-main
+
+// bottom-side
+
+///////////////////////////////////////////////////
+// Footer
+///////////////////////////////////////////////////
+
+?>
+
+
+
+
+
+
+
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
