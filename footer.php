c80aafb3b00e46900bd6be796b7508a17d10d079
Cameron Browning <camb416@gmail.com>
2017-01-31 00:10:03 -0500

    adjusted turnpages

--- footer.php
+++ footer.php
@@ -25,6 +25,14 @@
 
 			<?php wp_nav_menu( array( 'theme_location' => 'footer-menu' ) ); ?>
 
+            <form role="search" method="get" class="search-form" action="/">
+                <label>
+                    <span class="screen-reader-text">Search for:</span>
+                    <input type="search" class="search-field" placeholder="Search …" value="" name="s">
+                </label>
+                <input type="submit" class="search-submit" value="Search">
+            </form>
+
             <a class="footerlogo" href="<?php echo site_url(); ?>" rel="home">Fantastics</a>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
